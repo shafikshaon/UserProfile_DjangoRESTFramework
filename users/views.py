0970ceaa64a5fbcdb415328ac76451a29b7ead5f
Shafik Shaon <shafikshaon@gmail.com>
2018-05-04 23:23:34 +0600

    add search profile feature

--- users/views.py
+++ users/views.py
@@ -4,6 +4,7 @@ from rest_framework import viewsets
 from rest_framework.response import Response
 from rest_framework import status
 from rest_framework.authentication import TokenAuthentication
+from rest_framework import filters
 
 from . import serializers, models, permissions
 
@@ -88,3 +89,5 @@ class UserViewSet(viewsets.ModelViewSet):
     queryset = models.User.objects.all()
     authentication_classes = (TokenAuthentication,)
     permission_classes = (permissions.UpdateOwnProfile,)
+    filter_backends = (filters.SearchFilter,)
+    search_fields = ('first_name', 'last_name', 'email',)
