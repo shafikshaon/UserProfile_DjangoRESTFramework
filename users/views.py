d83f3fb84f4854b8b2eb937ba3fa34cbcccc65aa
Shafik Shaon <shafikshaon@gmail.com>
2018-05-04 11:39:23 +0600

    api sample post method implementation for testing

--- users/views.py
+++ users/views.py
@@ -1,11 +1,16 @@
 from django.shortcuts import render
 from rest_framework.views import APIView
 from rest_framework.response import Response
+from rest_framework import status
+
+from . import serializers
 
 # Create your views here.
 
 
 class HelloAPIView(APIView):
+    serializer_class = serializers.HelloSerilizer
+
     def get(self, request, format=None):
         api_view = [
             'Hello',
@@ -14,3 +19,15 @@ class HelloAPIView(APIView):
         ]
 
         return Response({'message': 'Woooo!', 'api_view': api_view})
+
+    def post(self, request):
+
+        serializer = serializers.HelloSerilizer(data=request.data)
+
+        if serializer.is_valid():
+            name = serializer.data.get('name')
+            message = 'Hello {0}'.format(name)
+            return Response({'message': message})
+        else:
+            return Response(
+                serializer.errors, status=status.HTTP_400_BAD_REQUEST)
