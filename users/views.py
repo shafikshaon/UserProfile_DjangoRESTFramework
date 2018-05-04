ced09ff9b6b97c1b6447d4718e343d61301c8f6c
Shafik Shaon <shafikshaon@gmail.com>
2018-05-04 12:06:53 +0600

    put, patch, delete method basic implementation for testing

--- users/views.py
+++ users/views.py
@@ -31,3 +31,12 @@ class HelloAPIView(APIView):
         else:
             return Response(
                 serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    def put(self, request, pk=None):
+        return Response({'method': 'put'})
+
+    def patch(self, request, pk=None):
+        return Response({'method': 'patch'})
+
+    def delete(self, request, pk=None):
+        return Response({'method': 'delete'})
